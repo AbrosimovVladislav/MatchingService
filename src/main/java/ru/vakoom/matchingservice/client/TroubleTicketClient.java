93b693a1a30d895cfe620651a6665fa391a2c5b2
AbrosimovVladislav <abrosimov94vladislav@gmail.com>
2020-05-12 17:48:59 +0300

    YH-26: changes for troubleTicket flow

--- src/main/java/ru/vakoom/matchingservice/client/TroubleTicketClient.java
+++ src/main/java/ru/vakoom/matchingservice/client/TroubleTicketClient.java
@@ -1,16 +1,37 @@
 package ru.vakoom.matchingservice.client;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import ru.vakoom.matchingservice.model.Product;
 import ru.vakoom.matchingservice.model.ScrapperOffer;
+import ru.vakoom.matchingservice.model.Ticket;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class TroubleTicketClient {
 
+    public static final String TT_BASE_PATH = "http://localhost:8083";
+    public static final String TT_SEND_TICKET_PATH = "/tickets";
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
     public void sendToTroubleTicket(ScrapperOffer scrapperOffer, List<Product> products) {
-        //ToDo implement
+        String url = TT_BASE_PATH + TT_SEND_TICKET_PATH;
+        restTemplate.exchange(
+                url,
+                HttpMethod.POST,
+                new HttpEntity<>(new Ticket(scrapperOffer, products, LocalDateTime.now())),
+                new ParameterizedTypeReference<>() {
+                }
+        );
+
     }
 
 }
